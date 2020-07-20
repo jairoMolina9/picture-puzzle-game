2cf21ab448a6b0d035e17fb44d36f3c302edf1a8
jairoMolina9 <molinaandres9991@gmail.com>
2020-07-20 18:37:10 -0400

    added game support

--- custom.js
+++ custom.js
@@ -5,6 +5,7 @@ function makeGrid() {
 
   var numbers = [];
 
+  //random number with no duplicates
   for(var x = 1; x <= 9; x++) {
     var img_numb = Math.floor(Math.random() * 9) + 1;
 
@@ -14,7 +15,8 @@ function makeGrid() {
 
     numbers.push(img_numb);
 
-    grid.innerHTML += ("<label><div> <input class = 'img-checkbox' type='checkbox' name='img' value='"+img_numb+"'> <img id='"+img_numb+"' src = 'img/"+img_numb+".png'> </div></label>");
+    //creates grid of images, checkboxes and labels
+    grid.innerHTML += ("<label><div><span id = '"+img_numb+"-span'  class = 'img-numb'>"+img_numb+"</span><input class = 'img-checkbox' type='checkbox' name='img' value='"+img_numb+"'> <img id='"+img_numb+"' src = 'img/"+img_numb+".png'> </div></label>");
   }
 }
 
@@ -25,6 +27,7 @@ function swapImages() {
 
   var checkboxes = document.getElementsByClassName('img-checkbox');
 
+  //gets the value of the checkboxes checked
   for(var i=0; checkboxes[i]; ++i){
       if(checkboxes[i].checked){
            imgChecked.push(checkboxes[i].value);
@@ -35,13 +38,17 @@ function swapImages() {
 if(imgChecked.length < 2) {
   alert("Must click at least 2 images")
 } else {
+
+  //change checkbox values
   var tmpval = checkboxes_html[0].value;
   checkboxes_html[0].value = checkboxes_html[1].value;
   checkboxes_html[1].value = tmpval;
 
+  //get images corresponding to checkbox checked
   var img_1 = document.getElementById(imgChecked[0]);
   var img_2 = document.getElementById(imgChecked[1]);
 
+  //change src and ids
   var tmp_src = img_1.src;
   var tmp_id = img_1.id;
 
@@ -51,7 +58,17 @@ if(imgChecked.length < 2) {
   img_1.id = img_2.id;
   img_2.id = tmp_id;
 
+
+
   moves++;
+
+  //update help numbers
+  document.getElementById(imgChecked[1]+"-span").id = imgChecked[0]+"-span";
+  document.getElementById(imgChecked[0]+"-span").id = imgChecked[1]+"-span";
+
+  document.getElementById(imgChecked[1]+"-span").innerHTML = imgChecked[1];
+  document.getElementById(imgChecked[0]+"-span").innerHTML = imgChecked[0];
+
 }
 
   document.getElementById("counter").innerHTML = "Moves:" + moves;
@@ -95,4 +112,16 @@ function endGame() {
   document.getElementById("swap").disabled = true;
   $('input[type=checkbox]').attr('disabled','true');
   document.getElementById("counter").innerHTML += "<span class = 'won-text'> YOU WON</span>";
+}
+
+function helpMe() {
+  var numbers = document.getElementsByClassName('img-numb');
+
+  for(var i=0; numbers[i]; ++i){
+    if(numbers[i].style.display == "block") {
+      numbers[i].style.display = "none";
+    } else {
+      numbers[i].style.display = "block";
+    }
+  }
 }
